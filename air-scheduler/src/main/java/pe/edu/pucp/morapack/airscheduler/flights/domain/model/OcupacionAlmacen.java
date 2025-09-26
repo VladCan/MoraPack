538dc0280866fb79841e270a06d1ac6c79d2a674
wolgan <danieljgjwolgan@gmail.com>
2025-09-25 23:50:55 -0500

    XD

--- air-scheduler/src/main/java/pe/edu/pucp/morapack/airscheduler/flights/domain/model/OcupacionAlmacen.java
+++
@@ -1,5 +0,0 @@
-package pe.edu.pucp.morapack.airscheduler.flights.domain.model;
-
-import java.time.Instant;
-
-public record OcupacionAlmacen(String aeropuerto, Instant desde, Instant hasta, int cantidad) {}
