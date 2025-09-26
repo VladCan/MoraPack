178178cab81bb0e553b0792ea2a49b314a146633
wolgan <danieljgjwolgan@gmail.com>
2025-09-25 23:50:42 -0500

    XD

---
+++ backend/src/main/java/pe/edu/pucp/morapack/airscheduler/scheduling/domain/service/alns/operators/RepairOperator.java
@@ -0,0 +1,10 @@
+package pe.edu.pucp.morapack.airscheduler.scheduling.domain.service.alns.operators;
+
+
+
+
+import pe.edu.pucp.morapack.airscheduler.scheduling.domain.model.SolucionProgramacion;
+
+public interface RepairOperator {
+    void repair(SolucionProgramacion s);
+}
