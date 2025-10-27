935cb282689bbc047c792974b2ecbca6e9506ab9
wolgan <danieljgjwolgan@gmail.com>
2025-10-27 18:05:47 -0500

    arreglamos testing

--- backend/src/main/java/pe/edu/pucp/morapack/airscheduler/engine/scheduling/run/RunManager.java
+++ backend/src/main/java/pe/edu/pucp/morapack/airscheduler/engine/scheduling/run/RunManager.java
@@ -284,7 +284,7 @@ public class RunManager {
                         
                         List<RepairOperator> reparadores = new ArrayList<>();
                         reparadores.add(new RegretRepair(2, new ArrayList<>(sedes), teg));
-                        reparadores.add(new SplitRepair(new ArrayList<>(sedes), teg, 50));
+                        reparadores.add(new SplitRepair(new ArrayList<>(sedes), teg));
                         
                         ALNS alns = new ALNS(teg, pedidosVentana, destructores, reparadores, wStart, ocupacionPorAeropuerto);
                         SolucionProgramacion solucionOptima = alns.ejecutar(seed);
