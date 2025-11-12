e3981949d24267bfb88cb326cde7a215c095e003
Fabián <fabianmr2806@hotmail.com>
2025-11-12 12:44:17 -0500

    ClockSwitcher v2 Done

--- frontend/src/components/common/ClockSwitcher.tsx
+++ frontend/src/components/common/ClockSwitcher.tsx
@@ -61,7 +61,7 @@ export default function ClockSwitcher({ className = "", running, finished, runNo
     {/* Reloj de simulación + tiempo transcurrido */}
     <div
       className={[
-        "absolute right-0 top-0 transition-all duration-300 ease-out",
+        "absolute right-0 top-0 transition-all duration-300 ease-out -mt-4",
         running ? "translate-y-[110%] opacity-100" : "translate-y-0 opacity-0",
       ].join(" ")}>
 
