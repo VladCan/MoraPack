42cab66722f2b7fd0f3e7390ce048bde74b2ba91
Fabián <fabianmr2806@hotmail.com>
2025-12-08 05:06:51 -0500

    b4 merge

--- backend/src/main/java/pe/edu/pucp/morapack/airscheduler/engine/scheduling/run/RunConfig.java
+++ backend/src/main/java/pe/edu/pucp/morapack/airscheduler/engine/scheduling/run/RunConfig.java
@@ -93,7 +93,7 @@ public class RunConfig {
                 sedes,
                 System.nanoTime(),
                 Duration.ofSeconds(60),
-                216//144
+                216//144 //a
         );
     }
 
