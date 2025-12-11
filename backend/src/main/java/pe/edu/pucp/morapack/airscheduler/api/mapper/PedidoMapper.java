695893676205e8192446be09e7babc02a85123ff
Fabián <fabianmr2806@hotmail.com>
2025-12-10 20:29:27 -0500

    OD bug fix

--- backend/src/main/java/pe/edu/pucp/morapack/airscheduler/api/mapper/PedidoMapper.java
+++ backend/src/main/java/pe/edu/pucp/morapack/airscheduler/api/mapper/PedidoMapper.java
@@ -4,6 +4,7 @@ import pe.edu.pucp.morapack.airscheduler.api.controllers.PedidosController;
 import pe.edu.pucp.morapack.airscheduler.engine.infrastructure.model.Pedido;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public final class PedidoMapper {
     //Estoy colocándolo aquí por efectos de brevedad (para que al crearse el pedido ya tenga su id)
@@ -32,6 +33,14 @@ public final class PedidoMapper {
                 fecha,
                 pedidoRequest.cantidad
         );
+    }
+
+    public static void reasignarIds(List<Pedido> pedidos) {
+        if (pedidos == null) return;
+        for (Pedido p : pedidos) {
+            if (p == null) continue;
+            p.setIdPedido(nextId++);   // <-- importante tener este setter
+        }
     }
 
 }
